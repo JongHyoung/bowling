c2b07e905bdd5cddd4e2ac8532a345a21813a63f
JongHyoung <ssdjjam447@gmail.com>
2026-05-26 00:26:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,29 @@
-const CACHE_NAME = 'bowling-v2';
-const URLS = [
-  '/bowling/',
-  '/bowling/index.html'
-];
+const CACHE_NAME = 'bowling-v3';
+const URLS = ['/bowling/', '/bowling/index.html'];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(URLS);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(cached) {
+    caches.match(e.request).then(cached => {
       if (cached) return cached;
-      return fetch(e.request).then(function(response) {
+      return fetch(e.request).then(res => {
         if (e.request.url.includes('index.html') || e.request.url.endsWith('/bowling/')) {
-          const clone = response.clone();
+          const clone = res.clone();
           caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
         }
-        return response;
-      }).catch(function() {
-        return caches.match('/bowling/index.html');
-      });
+        return res;
+      }).catch(() => caches.match('/bowling/index.html'));
     })
   );
 });
